4d98d46661ffe2c63cdbec637372f39158747614
Dima <dimarkens@gmail.com>
2022-03-24 14:10:49 -0700

    add invite checker

--- chat_project/chat/views.py
+++ chat_project/chat/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import login, logout
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import FormView, View
@@ -22,12 +22,15 @@ class LoginView(View):
         form = LoginForm()
         context = {"form": form}
         username = request.GET.get("username", None)
+
         if username:
             context.update({"method": "post", "username": username})
             is_user = User.objects.filter(username=username).exists()
             context["submit_text"] = self.login_text if is_user else self.signin_text
+
         else:
             context["method"] = "get"
+
         return render(request, 'chat/login.html', context)
 
     def post(self, request):
@@ -39,14 +42,18 @@ class LoginView(View):
 
         if form.is_valid():
             user, created = User.objects.get_or_create(username=username)
+
             if created:
                 user.set_password(password)
                 user.save()
                 login(request, user)
+
             elif not user.check_password(password):
                 context["submit_text"] = "Wrong password!"
                 return render(request, 'chat/login.html', context)
+
             login(request, user)
+
             return redirect(reverse("index"))
 
         context["validation_error"] = form.errors
@@ -65,8 +72,13 @@ class IndexView(LoginRequiredMixin, View):
 class ChatRoomView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         user = request.user
+        room_name = kwargs["room_name"]
+
+        has_invite = user.chatroom_invitations.filter(name=room_name).exists()
+        if not has_invite:
+            return HttpResponseBadRequest("You have no invite!")
         context = {
-            'room_name': kwargs["room_name"],
+            'room_name': room_name,
             'username': user.username,
         }
         return render(request, 'chat/room.html', context)
